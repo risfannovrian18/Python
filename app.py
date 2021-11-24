e0412867131e752bf5a8f5cb126c34a8ce9e0a8e
risfannovrian18 <risfan.unida@unida.ac.id>
2021-11-24 08:45:13 +0700

    edit secret_number

---
+++ app.py
@@ -0,0 +1,7 @@
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
--- secret_number.py
+++ secret_number.py
@@ -1,11 +1,19 @@
-secret_number = 8
+name = input('Masukan namamu : ')
+if len(name) < 3:
+    print("Nama harus lebih dari 3 karakter")
+elif len(name) > 50:
+    print("Nama tidak boleh lebih dari 50 karakter")
+else :
+    print("Nama yang bagus !!")
+#tebak angka
+secret_number = 7
 guess_count = 0
 guess_limit = 3
 while guess_count < guess_limit:
-    guess = int(input('Ayo tebak angka berapakah aku : '))
+    guess = int(input('Ayo tebak!! angka berapakah aku? : '))
     guess_count += 1
     if guess == secret_number:
-        print('Yeayy kamu menang !')
+        print(f"Yeayy {name} kamu menang :)")
         break
 else :
-    print("Maaf, kamu gagal :(")
+    print(f"Maaf, {name} Kamu kalah :(")
